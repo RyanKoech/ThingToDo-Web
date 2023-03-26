34a4c5c6b394d500ea751d56d6812c40a801485d
RyanKoech <sirryankoech@gmail.com>
2023-03-26 14:17:22 +0300

    set up firebase

---
+++ src/config/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLKXZNI4Qf4W1tQ4YP0lIOZiEC_8fYTH4",
+  authDomain: "react-to-do-14de5.firebaseapp.com",
+  projectId: "react-to-do-14de5",
+  storageBucket: "react-to-do-14de5.appspot.com",
+  messagingSenderId: "888693698547",
+  appId: "1:888693698547:web:29733d446267c121186c86"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
