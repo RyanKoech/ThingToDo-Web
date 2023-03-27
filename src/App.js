8a905b5be849bc4cac9f0aed5d0a03aeeb394e4d
RyanKoech <sirryankoech@gmail.com>
2023-03-27 15:05:08 +0300

    implement loading spinner

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function App() {
 
   const taskInputRef = useRef();
   const [thingsToDo, setThingsToDo] = useState(THINGS_TO_DO);
+  const [isLoading, setIsLoading] = useState(true);
+  
   const addThingToDo = (thingToDo) => {
     setThingsToDo([
       ...thingsToDo,
@@ -73,7 +75,22 @@ function App() {
           <label htmlFor="taskname" className="block mb-2 text-sm font-medium text-gray-900">Task Name</label>
           <input ref={taskInputRef} type="text" id="taskname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Walk the dog" required/>
         </div> 
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+        {
+          isLoading 
+          ? <div className="flex items-center justify-center">
+              <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status">
+                <span
+                  className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                  >Loading...</span
+                >
+              </div>
+            </div>
+
+          : <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+        }
+        
       </form>
 
       <div>
@@ -85,12 +102,6 @@ function App() {
               })
             : <div className="text-xl3 font-bold text-center">No tasks at the moment</div>
           }
-            {
-              
-              thingsToDo.map((thingToDo, index)=> {
-                return <ThingToDo key={thingToDo.id} onCheckboxClick={()=>{onCheckboxClick(thingToDo.id)}} taskName={thingToDo.name} done={thingToDo.done} onDeleteClick={() => removeThingToDo(thingToDo)}/>
-              })
-            }
         </ul>
       </div>
     </div>
